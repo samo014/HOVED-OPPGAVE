67a8a970ee0411b6bb551018e8bc4a8b76e984ab
SaidSheekh <saidmturner@gmail.com>
2023-12-07 18:27:52 +0100

    Late Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//CODE FOR FILTER. WRITE DOWN THE YEAR A PHOTO FROM SELECTED YEAR WILL SHOW//
+
+
+document.getElementById("filter").addEventListener("click", function () {
+	var year = prompt("Enter a year to filter by:");
+ 
+	var images = document.querySelectorAll("img");
+ 
+	images.forEach(function (image) {
+	  if (image.getAttribute("data-year") === year) {
+		 image.style.display = "block";
+	  } else {
+		 image.style.display = "none";
+	  }
+	});
+ });
